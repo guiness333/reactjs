9b990d0830debc6550a60770eb1b61b1b3d765d8
Walber Martins <walber123k@hotmail.com>
2020-02-25 00:42:29 -0300

    Updates

---
+++ precache-manifest.98fb9cb4e542484c096c36e165d2d7a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df69fbd7396c751ac5820a849dd26a4",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "cbf93b5846f19bfb6be4",
+    "url": "/reactjs/static/css/main.e34c46cd.chunk.css"
+  },
+  {
+    "revision": "a7e5a0863c45a1fc4da7",
+    "url": "/reactjs/static/js/2.22984308.chunk.js"
+  },
+  {
+    "revision": "cbf93b5846f19bfb6be4",
+    "url": "/reactjs/static/js/main.6ed63f44.chunk.js"
+  },
+  {
+    "revision": "a7038932f5a8db0e1f8e",
+    "url": "/reactjs/static/js/runtime-main.0f9e1837.js"
+  }
+]);
